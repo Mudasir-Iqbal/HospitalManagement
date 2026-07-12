6554213e11c46414147bc9af4ece782230add34c
Muhammad Mudasir <pc16967.muhammadmudasir@gmail.com>
2026-07-12 14:27:32 +0500

    Update staff.py

--- app/crud/staff.py
+++ app/crud/staff.py
@@ -17,3 +17,44 @@ def create_staff_in_db(db: Session, staff_data: StaffCreate):
     db.refresh(new_staff)
 
     return new_staff
+
+
+
+
+# 1. Pura data read karne ka function
+def get_all_staff_from_db(db: Session):
+    # SQL equivalent: SELECT * FROM staff;
+    return db.query(StaffModel).all()
+
+# 2. Specific ID se data read karne ka function
+def get_staff_by_id_from_db(db: Session, staff_id: int):
+    # SQL equivalent: SELECT * FROM doctors WHERE id = staff_id LIMIT 1;
+    return db.query(StaffModel).filter(StaffModel.id == staff_id).first()
+
+
+# 1. Update Karne Ka Function
+def update_staff_in_db(db: Session, staff_id: int, updated_data: StaffCreate):
+    # Pehle us staff ko dhoondho jise badalna hai
+    db_staff = db.query(StaffModel).filter(StaffModel.id == staff_id).first()
+    
+    if db_staff:
+        # Purane data ki jagah naya data set karo
+        db_staff.name = updated_data.name
+        db_staff.role = updated_data.role
+        db_staff.shift = updated_data.shift
+        db_staff.salary = updated_data.salary
+        
+        db.commit()        # Changes ko save kiya
+        db.refresh(db_staff) # Object ko sync kiya
+    return db_staff
+
+
+# 2. Delete Karne Ka Function
+def delete_doctor_from_db(db: Session, staff_id: int):
+    db_staff = db.query(StaffModel).filter(StaffModel.id == staff_id).first()
+    
+    if db_staff:
+        db.delete(db_staff) # Database state se delete kiya
+        db.commit()          # Permanent delete confirm kiya
+        return True
+    return False
