893d0d2a82116aea4f6488789c5c3c44c674d389
Muhammad Mudasir <pc16967.muhammadmudasir@gmail.com>
2026-07-12 13:10:43 +0500

    get data

--- app/crud/doctor.py
+++ app/crud/doctor.py
@@ -15,3 +15,14 @@ def create_doctor_in_db(db: Session, doctor_data: DoctorCreate):
     db.refresh(new_doctor)  # 4. ID automatic generate ho kar object mein load ho jayegi
     
     return new_doctor
+
+
+# 1. Pura data read karne ka function
+def get_all_doctors_from_db(db: Session):
+    # SQL equivalent: SELECT * FROM doctors;
+    return db.query(DoctorModel).all()
+
+# 2. Specific ID se data read karne ka function
+def get_doctor_by_id_from_db(db: Session, doctor_id: int):
+    # SQL equivalent: SELECT * FROM doctors WHERE id = doctor_id LIMIT 1;
+    return db.query(DoctorModel).filter(DoctorModel.id == doctor_id).first()
