06260521aee1b9c530feea5f0aca440eecdac031
Muhammad Mudasir <pc16967.muhammadmudasir@gmail.com>
2026-07-12 13:44:26 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import doctor # doctor router ko import kar rahy hay
+from app.routes import doctor,patient # doctor router ko import kar rahy hay
 from app.database import engine, Base
 from app.models import doctor as doctor_model # Model ko import karna zaroori hai taake Base ko pata chale
 from app.models import patient as patient_model
@@ -12,6 +12,7 @@ app = FastAPI(title="Hospital Management System")
 
 # Counter ko main gate ke sath connect kar diya
 app.include_router(doctor.router)
+app.include_router(patient.router)
 
 @app.get("/")
 def home():
