8a2c3a14a459bbf293d6bb8da0ba7f75f261e0cc
Muhammad Mudasir <pc16967.muhammadmudasir@gmail.com>
2026-07-12 14:44:01 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.routes import doctor,patient # doctor router ko import kar rahy hay
+from app.routes import doctor,patient,staff # doctor router ko import kar rahy hay
 from app.database import engine, Base
 from app.models import doctor as doctor_model # Model ko import karna zaroori hai taake Base ko pata chale
 from app.models import patient as patient_model
+from app.models import staff as staff_model
 
 
 # Ye line server start hote hi models ko check karegi aur PostgreSQL mein tables bana degi!
@@ -14,6 +15,7 @@ app = FastAPI(title="Hospital Management System")
 # Counter ko main gate ke sath connect kar diya
 app.include_router(doctor.router)
 app.include_router(patient.router)
+app.include_router(staff.router)
 
 @app.get("/")
 def home():
