e5c37782a8cc8029a13616d4dd8619e56c91c86b
Muhammad Mudasir <pc16967.muhammadmudasir@gmail.com>
2026-07-12 13:32:09 +0500

    Update main.py
    import patient_model

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.routes import doctor # doctor router ko import kar rahy hay
 from app.database import engine, Base
 from app.models import doctor as doctor_model # Model ko import karna zaroori hai taake Base ko pata chale
+from app.models import patient as patient_model
+
 
 # Ye line server start hote hi models ko check karegi aur PostgreSQL mein tables bana degi!
 Base.metadata.create_all(bind=engine)
