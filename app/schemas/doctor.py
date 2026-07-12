7b42a8e72b8854a6125e5e2d4c9575b38dded33f
Muhammad Mudasir <pc16967.muhammadmudasir@gmail.com>
2026-07-12 12:20:32 +0500

    blue print
    yaha hum basemodel ko kah rahy kay humy name email specialization ye chahyee

---
+++ app/schemas/doctor.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+# Humne aik blueprint (schema) banaya naye doctor ke form ke liye
+class DoctorCreate(BaseModel):
+    name: str
+    specialization: str
+    email: str
